20c5f16bae7dd35d6947d8365ebf2f83864fa69d
Masakazu Miyamoto <masakazumiyamoto@MasakazunoMac-mini.local>
2020-04-24 16:11:01 +0900

    minor rivise

--- ROM.vhd
+++ ROM.vhd
@@ -5,10 +5,9 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity OUTPUT_PATCH is
-	port
-	(
-		ADDR		: in natural range 0 to 50;
-		CLK		: in std_logic;
+	port (
+		ADDR	: in 	natural range 0 to 50;
+		CLK		: in 	std_logic;
 		Q			: out std_logic_vector(7 downto 0)
 	);
 
@@ -17,12 +16,13 @@ end entity;
 architecture RTL of OUTPUT_PATCH is
 
 	-- Build a 2-D array type for the patch table
-	subtype WORD_T is std_logic_vector(7 downto 0);
-	type MEMORY_T is array(50 downto 0) of WORD_T;
+	subtype WORD_T 	is std_logic_vector(7 downto 0);
+		type MEMORY_T 	is array(50 downto 0) of WORD_T;
 
 	signal ROM:MEMORY_T;
-	attribute ram_init_file:string;
-	attribute ram_init_file of ROM: signal is "patch_table.mif";
+	attribute ram_init_file : string;
+	attribute ram_init_file of ROM :
+		signal is "patch_table.mif";
 	-- ram_init_fileはファイルを指定するための特別なアトリビュート名
 
 begin
